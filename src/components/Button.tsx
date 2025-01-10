21089fdfb9d74a287a3dddce906aa41c40add777
Zhao Changlin <huskydawg@foxmail.com>
2025-01-10 23:21:34 +0800

    refactor Button component to improve color prop formatting and maintain default value

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,19 @@ import React from "react";
 
 interface Props {
   children: string;
-  color?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark';
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark";
   onClick: () => void;
 }
 
-const Button = ({ children, onClick, color = 'primary' }: Props) => {
+const Button = ({ children, onClick, color = "primary" }: Props) => {
   return (
     <button className={"btn btn-" + color} onClick={onClick}>
       {children}
