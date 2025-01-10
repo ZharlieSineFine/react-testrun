b003e6d006f56770ffb26cdbf4792d55ece21f32
Zhao Changlin <huskydawg@foxmail.com>
2025-01-10 23:20:07 +0800

    add onClose handler to Alert component and update App to support dismiss functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false)
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      { alertVisible && <Alert>
-        My alert
-      </Alert>}
-      <Button color='primary' onClick={() => setAlertVisibility(true)}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
         My Button
       </Button>
     </div>
